8647b8115e3fde70e1ee631e81c73ef865ebe4f1
stevenAnto <ecalcinap@unsa.edu.pe>
2022-10-08 11:23:32 -0500

    imprimir referencia, valor en 4 casos

--- Apuntadores.cpp
+++ Apuntadores.cpp
@@ -2,9 +2,16 @@
 using namespace std;
 
 int main(){
-  int y = 5;
+
+  int a = 7;
+  int *aPtr = &a; //Paso la referencia
+  cout << "la direccion de a es " << &a
+    << "\n El valor de aPtr es " << aPtr;
+  cout << "\n El valor de a es " << a
+    << "\nEl valor de aPtr es" << *aPtr<<endl;
+  /*int y = 5;
   int *yPtr= nullptr;
   yPtr = &y;
   *yPtr = 10;
-  cout << "que imprime "<< *yPtr<<endl;
+  cout << "que imprime "<< *yPtr<<endl;*/
 }
