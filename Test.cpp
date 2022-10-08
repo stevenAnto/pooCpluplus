1981593d34486dcd891c3d154ca3cece83e8505d
stevenAnto <ecalcinap@unsa.edu.pe>
2022-10-07 20:27:45 -0500

    uso de vectore

--- Test.cpp
+++ Test.cpp
@@ -1,13 +1,29 @@
 #include<iostream>
+#include<vector>
 #include "LibroCalificaciones.h"
 
 using namespace std;
+ void imprimirVector(const vector <int> &);
+ void recibirVector( vector< int > &);
 
 int main(){
-  LibroCalificaciones libro1("primero");
-  LibroCalificaciones libro2("segundo");
-
-  cout << "Creando los Libros"
-    << libro1.obtenerNombreCurso()<<endl;
-
+  vector< int > enteros1(7);
+  recibirVector (enteros1);
+  imprimirVector(enteros1);
 }
+
+  void recibirVector( vector< int > &arreglo){
+    for (int &elemento :  arreglo)
+      cin >> elemento;
+  }
+  void imprimirVector(const vector < int > &arreglo){
+    for (int elemento : arreglo)
+      cout << elemento << " ";
+    cout << endl;
+  }
+  //LibroCalificaciones libro1("primero");
+  //LibroCalificaciones libro2("segundo");
+
+  //cout << "Creando los Libros"
+   // << libro1.obtenerNombreCurso()<<endl;
+
