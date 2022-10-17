8a70103521e01b34563b5613ffed60fa27707b7b
stevenAnto <ecalcinap@unsa.edu.pe>
2022-10-17 18:08:30 -0500

    como ingresar la cadena de caracteres

---
+++ workspace/caracteres.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string.h>
+
+using namespace std;
+
+int main(){
+  char palabra[]=
+    "hola";
+  string palabra2 = "palabra";
+  int longitud =0;
+  longitud = strlen(palabra2);
+  cout << longitud<<endl;
+  return 0;
+}
