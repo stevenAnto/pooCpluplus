837df9f02f360c461dd386d3c36724ccb307d4eb
stevenAnto <ecalcinap@unsa.edu.pe>
2022-10-25 15:18:25 -0500

    multiplicacion22 strassen

--- workspace/matrizPuntero.cpp
+++ workspace/matrizPuntero.cpp
@@ -1,3 +1,10 @@
+/*Se uso c++ con edito Vim
+ * Librerias iostream y stdlib.h para matrices dinamicas
+ * Nombre: Esteven Antonio Calcina Puma
+ * CUI :20074496
+ * GUIA DE LABORATORIO N`4
+ * Ejercicio 1
+ */
 #include<iostream>
 #include<stdlib.h>
 using namespace std;
@@ -5,17 +12,19 @@ using namespace std;
 
 void pedirDatos();
 void imprimir(int **,int , int );
-void sumarMatrices(int **,int **,int, int);
-int **puntero_matriz1,**puntero_matriz2,nfilas,ncol;
+void sumarMatrices(int **,int **,int **,int, int);
+void multiMatriz22(int **,int **,int **,int,int);
+int **puntero_matriz1,**puntero_matriz2,nfilas,ncol, **puntero_matrizr;
 
 int main(){
   pedirDatos();
-  sumarMatrices(puntero_matriz1,puntero_matriz2,nfilas,ncol);
-  imprimir(puntero_matriz1,nfilas,ncol);
+//  sumarMatrices(puntero_matriz1,puntero_matriz2,puntero_matrizr,nfilas,ncol);
+  multiMatriz22(puntero_matriz1,puntero_matriz2,puntero_matrizr,nfilas,ncol);
+  imprimir(puntero_matrizr,nfilas,ncol);
   for(int i=0;i<nfilas;i++){
-    delete[] puntero_matriz1[i];
+    delete[] puntero_matrizr[i];
   }
-  delete[] puntero_matriz1;
+  delete[] puntero_matrizr;
   //falta liberar memoria para la 2
   return 0;
 }
@@ -55,14 +64,32 @@ void pedirDatos(){
       cin >>*(*(puntero_matriz2+i)+j);
     }
   }
+  ////Aqui se llenara la nueva matriz
+  puntero_matrizr = new int*[nfilas]; //reservando memoria para las filas
+  for (int i=0;i<nfilas;i++){
+    puntero_matrizr[i] = new int[ncol]; //reservando memeoria para laas columnas
+  }
 }
-void sumarMatrices(int **puntero_matriz1,int **puntero_matriz2, int nfilas, int ncolu){
+void sumarMatrices(int **puntero_matriz1,int **puntero_matriz2,int **puntero_matrizr, int nfilas, int ncolu){
   for(int i=0;i<nfilas;i++){
     for(int j=0;j<ncol;j++){
-      *(*(puntero_matriz1+i)+j) += *(*(puntero_matriz2+i)+j);
+      *(*(puntero_matrizr+i)+j) = *(*(puntero_matriz1+i)+j)+*(*(puntero_matriz2+i)+j);
+     // *(*(puntero_matriz1+i)+j) += *(*(puntero_matriz2+i)+j);
     }
-
   }
+}
+void multiMatriz22(int **a,int **b,int **puntero_matrizr, int nfilas, int ncolu){
+  int m1=( a[0][0]+a[1][1])*(b[0][0]+b[1][1]);
+  int m2=( a[1][0]+a[1][1])*(b[0][0]);
+  int m3=( a[0][0])*(b[0][1]-b[1][1]);
+  int m4=( a[1][1])*(b[1][0]-b[0][0]);
+  int m5=( a[0][0]+a[0][1])*(b[1][1]);
+  int m6=( a[1][0]-a[0][0])*(b[0][0]+b[0][1]);
+  int m7=( a[0][1]-a[1][1])*(b[1][0]+b[1][1]);
+  puntero_matrizr[0][0]=m1+m4-m5+m7;
+  puntero_matrizr[0][1]=m3+m5;
+  puntero_matrizr[1][0]=m2+m4;
+  puntero_matrizr[1][1]=m1+m3-m2+m6;
 }
 void imprimir ( int **puntero_matriz, int nfilas, int ncol){
   for(int i=0;i<nfilas;i++){
