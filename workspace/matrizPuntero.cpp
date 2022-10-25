03a341f0beb562e21bce20f33b6a2ff72b30a788
stevenAnto <ecalcinap@unsa.edu.pe>
2022-10-25 06:57:11 -0500

    sumaMatrices

--- workspace/matrizPuntero.cpp
+++ workspace/matrizPuntero.cpp
@@ -5,34 +5,63 @@ using namespace std;
 
 void pedirDatos();
 void imprimir(int **,int , int );
-int **puntero_matriz,nfilas,ncol;
+void sumarMatrices(int **,int **,int, int);
+int **puntero_matriz1,**puntero_matriz2,nfilas,ncol;
 
 int main(){
   pedirDatos();
-  imprimir(puntero_matriz,nfilas,ncol);
+  sumarMatrices(puntero_matriz1,puntero_matriz2,nfilas,ncol);
+  imprimir(puntero_matriz1,nfilas,ncol);
   for(int i=0;i<nfilas;i++){
-    delete[] puntero_matriz[i];
+    delete[] puntero_matriz1[i];
   }
-  delete[] puntero_matriz;
+  delete[] puntero_matriz1;
+  //falta liberar memoria para la 2
   return 0;
 }
 
 void pedirDatos(){
+  cout<<"Llenando matriz1\n";
   cout << "digite el numero de filas";
   cin >> nfilas;
   cout << "digite el numero de columnas";
   cin >> ncol;
 
-  puntero_matriz = new int*[nfilas]; //reservando memoria para las filas
+  puntero_matriz1 = new int*[nfilas]; //reservando memoria para las filas
   for (int i=0;i<nfilas;i++){
-    puntero_matriz[i] = new int[ncol]; //reservando memeoria para laas columnas
+    puntero_matriz1[i] = new int[ncol]; //reservando memeoria para laas columnas
   }
   cout <<"\n digitando elementos de la matriz";
   for (int i=0;i<nfilas;i++){
     for (int j=0;j<ncol;j++){
       cout << " ingrese datos"<<"["<<i<<"]"<<"["<<j<<"]";
-      cin >>*(*(puntero_matriz+i)+j);
+      cin >>*(*(puntero_matriz1+i)+j);
     }
+  }
+  cout<<"\nLlenando para la segunda matriz\n";
+  cout << "digite el numero de filas";
+  cin >> nfilas;
+  cout << "digite el numero de columnas";
+  cin >> ncol;
+
+  puntero_matriz2 = new int*[nfilas]; //reservando memoria para las filas
+  for (int i=0;i<nfilas;i++){
+    puntero_matriz2[i] = new int[ncol]; //reservando memeoria para laas columnas
+  }
+  cout <<"\n digitando elementos de la matriz";
+  for (int i=0;i<nfilas;i++){
+    for (int j=0;j<ncol;j++){
+      cout << " ingrese datos"<<"["<<i<<"]"<<"["<<j<<"]";
+      cin >>*(*(puntero_matriz2+i)+j);
+    }
+  }
+}
+void sumarMatrices(int **puntero_matriz1,int **puntero_matriz2, int nfilas, int ncolu){
+  for(int i=0;i<nfilas;i++){
+    for(int j=0;j<ncol;j++){
+      *(*(puntero_matriz1+i)+j) += *(*(puntero_matriz2+i)+j);
+    }
+
   }
 }
 void imprimir ( int **puntero_matriz, int nfilas, int ncol){
