7b8cf539925c6421c080754365261bdb681f5f98
stevenAnto <ecalcinap@unsa.edu.pe>
2022-11-01 17:58:45 -0500

    muestroTotal

--- voraz/mochilaVoraz.cpp
+++ voraz/mochilaVoraz.cpp
@@ -7,6 +7,7 @@ double* procesarVectores(int*,int*,int);
 void imprimir(int*,int);
 void imprimird(double*,int);
 int maxV(double*,int,int);
+void productoPunto(double*,int*,int);
 int *valores,*pesos;
 int main(){
   int numObj=0;
@@ -14,7 +15,9 @@ int main(){
   pedirDatos(numObj,maxPeso);
   double *solucion;
   solucion = voraznochila(numObj,pesos,valores,maxPeso);
+  cout << "la combinacion es :"<<endl;
   imprimird(solucion,numObj);
+  productoPunto(solucion,valores,numObj);
 
 }
 double * voraznochila(int n, int *pesos,int *valor,int maxPeso){
@@ -23,7 +26,7 @@ double * voraznochila(int n, int *pesos,int *valor,int maxPeso){
   double *solucion = new double[n];
   double *valorXpeso = procesarVectores(valor,pesos,n);
   cout <<"vectorProcesado"<<endl;
-  imprimird(valorXpeso,n);
+  //imprimird(valorXpeso,n);
   for(int i=0;i<n;i++){
     *(solucion+i)=0;
   }
@@ -35,13 +38,13 @@ double * voraznochila(int n, int *pesos,int *valor,int maxPeso){
     if( pesoActual+pesos[maxVP]<=maxPeso){
       pesoActual+=pesos[maxVP];
       cout<<"peasoActual"<<pesoActual<<endl;
-      solucion[maxVP]=1;
-      valorXpeso[maxVP]=0;//para que ya no lo tome en cuenta
-  cout <<"vectorProcesado"<<endl;
-      imprimird(valorXpeso,n);
+      *(solucion+maxVP)=1;
+      *(valorXpeso+maxVP)=1;
+      cout <<"vectorProcesado"<<endl;
+      //   imprimird(valorXpeso,n);
     }
     else{
-      solucion[maxVP]=((float)(maxPeso-pesoActual)/(float)pesos[maxVP]);
+      *(solucion+maxVP)=((float)(maxPeso-pesoActual)/(float)pesos[maxVP]);
       pesoActual=maxPeso;
     }
   }
@@ -57,11 +60,9 @@ double * procesarVectores(int *v1,int *v2,int length){
   return nuevoVector;
 }
 int maxV(double *v,int separar,int n){
-  cout<<
-    "separa"<<separar<<endl;
   int max=0;
   for (int i=0;i<n;i++){
-    if(v[i]>v[max]){
+    if(*(v+i)>*(v+max)){
       max=i;
     };
   }
@@ -91,9 +92,17 @@ void imprimir(int *v, int l ){
   cout<<endl;
 }
 void imprimird(double *v, int l ){
-  cout<<"inimpriD"<<endl;
+  cout<<"[";
   for(int i=0;i<l;i++){
-    cout <<v[i]<<",";
+    cout <<v[i]<<";";
   }
+  cout<<"]";
   cout<<endl;
+}
+void productoPunto(double *v1,int* v2,int n){
+  double suma=0;
+  for (int i=1;i<n;i++){
+    suma += (v1[i]*v2[i]);
+  }
+  cout<<"total valor"<<suma<<endl;
 }
