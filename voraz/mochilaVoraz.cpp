00faecdfd913b56cf2962b1085a5c609bab28596
stevenAnto <ecalcinap@unsa.edu.pe>
2022-11-01 17:41:47 -0500

    problema de la mochila , resulto Vorazmente

---
+++ voraz/mochilaVoraz.cpp
@@ -0,0 +1,99 @@
+#include<iostream>
+using namespace std;
+
+void pedirDatos(int&,int&);
+double* voraznochila(int,int*,int*,int);
+double* procesarVectores(int*,int*,int);
+void imprimir(int*,int);
+void imprimird(double*,int);
+int maxV(double*,int,int);
+int *valores,*pesos;
+int main(){
+  int numObj=0;
+  int maxPeso;
+  pedirDatos(numObj,maxPeso);
+  double *solucion;
+  solucion = voraznochila(numObj,pesos,valores,maxPeso);
+  imprimird(solucion,numObj);
+
+}
+double * voraznochila(int n, int *pesos,int *valor,int maxPeso){
+  cout<<"inVorz"<<endl;
+  double pesoActual=0;
+  double *solucion = new double[n];
+  double *valorXpeso = procesarVectores(valor,pesos,n);
+  cout <<"vectorProcesado"<<endl;
+  imprimird(valorXpeso,n);
+  for(int i=0;i<n;i++){
+    *(solucion+i)=0;
+  }
+  cout<<"vestorSolucioninicial"<<endl;
+  imprimird(solucion,n);
+  int maxVP=-1;//indice del mejor
+  while(pesoActual<maxPeso){
+    maxVP= maxV(valorXpeso,maxVP,n);//
+    if( pesoActual+pesos[maxVP]<=maxPeso){
+      pesoActual+=pesos[maxVP];
+      cout<<"peasoActual"<<pesoActual<<endl;
+      solucion[maxVP]=1;
+      valorXpeso[maxVP]=0;//para que ya no lo tome en cuenta
+  cout <<"vectorProcesado"<<endl;
+      imprimird(valorXpeso,n);
+    }
+    else{
+      solucion[maxVP]=((float)(maxPeso-pesoActual)/(float)pesos[maxVP]);
+      pesoActual=maxPeso;
+    }
+  }
+  return solucion;
+}
+double * procesarVectores(int *v1,int *v2,int length){
+  cout<<"inProcesar"<<endl;
+  double * nuevoVector= new double[length];
+  for (int i=0;i<length;i++){
+    *(nuevoVector+i)=(float)(*(v1+i))/(float)(*(v2+i));
+  }
+  //imprimird(nuevoVector,length);
+  return nuevoVector;
+}
+int maxV(double *v,int separar,int n){
+  cout<<
+    "separa"<<separar<<endl;
+  int max=0;
+  for (int i=0;i<n;i++){
+    if(v[i]>v[max]){
+      max=i;
+    };
+  }
+  cout <<"max: "<<max<<endl;
+  return max;
+}
+void pedirDatos(int &n,int &p){
+  cout<<"cuantos objetos seran"<<endl;
+  cin >>n; 
+  cout<<"cuantos  seran el peso maximo"<<endl;
+  cin >>p; 
+  valores = new int[n];
+  pesos = new int[n];
+  for (int i=0;i<n;i++){
+    cout <<"valor para "<<i<<endl;
+    cin>>valores[i];
+    cout <<"peso para "<<i<<endl;
+    cin>>pesos[i];
+  }
+  imprimir(valores,n);
+  imprimir(pesos,n);
+}
+void imprimir(int *v, int l ){
+  for(int i=0;i<l;i++){
+    cout <<v[i]<<",";
+  }
+  cout<<endl;
+}
+void imprimird(double *v, int l ){
+  cout<<"inimpriD"<<endl;
+  for(int i=0;i<l;i++){
+    cout <<v[i]<<",";
+  }
+  cout<<endl;
+}
