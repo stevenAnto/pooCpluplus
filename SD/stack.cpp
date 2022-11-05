8576d5928ed55b0fcaebdc5f151e95864bb9ea38
stevenAnto <ecalcinap@unsa.edu.pe>
2022-11-05 12:15:21 -0500

    SEGUNDO MODO STACK

--- SD/stack.cpp
+++ SD/stack.cpp
@@ -7,30 +7,27 @@ struct nodo{
   int dato;
   nodo *puntero;
 };
+void push(nodo **,int);
 
 
 int main(){
-  nodo inicio;
-  inicio.dato=0;
-  inicio.puntero=NULL;
-  nodo nodo1;
-  nodo1.dato =5;
-  nodo1.puntero = &inicio;
-  inicio = nodo1;
-  cout <<"que tiene incio:"<<inicio.dato<<endl;
-  cout <<"que tiene nodo1:"<<nodo1.dato<<endl;
-  cout <<"que tiene incio:"<<inicio.puntero<<endl;
-  cout <<"que tiene nodo1:"<<nodo1.puntero<<endl;
-  //No se puede asignar null a una estructura, por lo tanto si deberiamos trabajar con puntero
-  /*Creo un nuevo nodo q y le pongo los datos, hago que su enlace apunte a p, y luego hago que p apunte a q*/
   nodo *pila=NULL;
-  nodo *nuevoNodo = new nodo();
-  cout <<pila<<endl;
-  //Por que no crear un nodo comun y corriente y no un nodo puntero;
+  cout<<pila<<endl;
+  int n1,n2;
+  cout<<
+    "ingrese un numero";
+  cin>>n1;
+  push(&pila,n1);
+  cout<<pila<<endl;
+  return 0;
 }
-void push(nodo *pila, int n){
-  nodo *nuevoNode = new nodo();
-  nuevoNode->dato = n;
-  nuevoNode->puntero = pila;
-  pila = nuevoNode;
+void push(nodo **pila, int n){
+  nodo *nuevoNodo = new nodo();
+  cout<<"direccion de nuevoNodo"<<nuevoNodo<<endl;
+  cout<<"direccion de pila"<<pila<<endl;
+  nuevoNodo->dato =n;
+  nuevoNodo->puntero =*pila;
+  *pila = nuevoNodo;
+  cout<<"direccion de nuevoNodo"<<nuevoNodo<<endl;
+  cout<<"direccion de pila"<<pila<<endl;
 }
